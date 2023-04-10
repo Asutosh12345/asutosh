78daca04d6c42cf4ca4a880256eb03f411b1a340
Asutosh12345 <122336109+Asutosh12345@users.noreply.github.com>
2023-04-10 07:55:05 -0700

    Add files via upload

---
+++ getMaxocc.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+
+char getMaxOccCharacter (string s) {
+    int arr[26] = {0};
+
+
+// creat an array of count of characters
+    for(int i=0; i<s.length(); i++) {
+        char ch = s[i];
+        int number = 0;
+        number = ch-'a';
+        arr[number] ++;
+
+    }
+
+        int maxi = -1,ans = 0;
+
+        for(int i = 0; i<26; i++) {
+            if(maxi < arr[i]) {
+                ans = i;
+                maxi = arr[i];
+            }
+        }
+        int finalans = 'a'+ans;
+        return  finalans;
+
+
+}
+int main () {
+        string s;
+        cin >> s;
+
+       cout << getMaxOccCharacter(s) << endl;
+}
